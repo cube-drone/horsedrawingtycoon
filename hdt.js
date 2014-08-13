7ede3e3e7f2cac18c7d6a13630668becfe63434e
classam <curtis@lassam.net>
2014-08-13 15:48:49 -0700

    Version 1 finished

--- hdt.js
+++ hdt.js
@@ -1,5 +1,5 @@
 
-function horsecanvas(canvas_element, eventhandler){
+function horsecanvas(element, eventhandler){
     var canvas, stage;
     var context;
     var mode;
@@ -10,8 +10,9 @@ function horsecanvas(canvas_element, eventhandler){
     var color;
     var stroke;
     var stamps;
+    var hitarea;
 
-    canvas = canvas_element
+    canvas = element.find("canvas")[0];
     context = canvas.getContext("2d");
     color = "#000000";
     mode = "stroke";
@@ -26,6 +27,9 @@ function horsecanvas(canvas_element, eventhandler){
     drawingCanvas = new createjs.Shape();
     stage.addChild(drawingCanvas);
     stamps = []
+    
+    hitArea = new createjs.Shape();
+    hitArea.graphics.beginFill("#000").drawRect(0, 0, canvas.width, canvas.height);
 
     function handleMouseDown(event) {
         if( mode == 'stroke'){
@@ -57,17 +61,17 @@ function horsecanvas(canvas_element, eventhandler){
         if(mode == 'stroke'){
             stage.removeEventListener("stagemousemove" , handleMouseMove);
         }
-        else{
-            if(mode == 'stamp'){
-                console.log("STAMPIN", stamp_url);
-                var bitmap = new createjs.Bitmap(stamp_url);
-                bitmap.x = stage.mouseX;
-                bitmap.y = stage.mouseY;
-                stage.addChild(bitmap);
-                stage.update();
-                stamps.push(stamp_url);
-                eventhandler.emit("stamped", stamp_url);
-            }
+    }
+    function handleClick(event) {
+        if(mode == 'stamp'){
+            console.log("STAMPIN", stamp_url, stage.mouseX, stage.mouseY);
+            var bitmap = new createjs.Bitmap(stamp_url);
+            bitmap.x = stage.mouseX;
+            bitmap.y = stage.mouseY;
+            stage.addChild(bitmap);
+            stage.update();
+            stamps.push(stamp_url);
+            eventhandler.emit("stamped", stamp_url);
         }
     }
     
@@ -81,10 +85,29 @@ function horsecanvas(canvas_element, eventhandler){
         stroke = newsize;
     }
     function completeCanvas(){
+        var horsedollars = 1;
         var analysis = HorseRank.analyze(canvas, context);
+        var colorhistogram = analysis.colorhistogram;
+        for (var key in colorhistogram) {
+            if (colorhistogram.hasOwnProperty(key) && key !== '#FFFFFF') {
+                if( colorhistogram[key] > 40000 ){
+                    horsedollars = horsedollars + 2;
+                }
+                else{
+                    horsedollars = horsedollars + 1;
+                }
+            }
+        }
+        horsedollars = parseInt(horsedollars/3, 10);
+        if (horsedollars > 9){
+            horsedollars = 9;
+        }
         console.log(analysis);
+        console.log(horsedollars);
         var img = canvas.toDataURL("image/png");
-        eventhandler.emit('newImage', img, analysis, stamps);
+        eventhandler.emit('showBreakdown', img, analysis, horsedollars);
+        eventhandler.emit('newImage', img);
+        eventhandler.emit('addMoney', horsedollars);
         eventhandler.emit('clearCanvas');
     }
     function clearCanvas(){
@@ -103,6 +126,8 @@ function horsecanvas(canvas_element, eventhandler){
 
         stage.removeAllChildren();
         drawingCanvas = new createjs.Shape();
+        drawingCanvas.hitArea = hitArea;
+        drawingCanvas.on("click", handleClick);
         stage.addChild(drawingCanvas);
         
         stamps = [];
@@ -132,14 +157,23 @@ function horsecanvas(canvas_element, eventhandler){
 
     stage.addEventListener("stagemousedown", handleMouseDown);
     stage.addEventListener("stagemouseup", handleMouseUp);
+    drawingCanvas.hitArea = hitArea;
+    drawingCanvas.on("click", handleClick);
     stage.update();
-   
+    
+    $(element).find("#done").click(function(){
+        eventhandler.emit("completeCanvas");
+    });
+
+    $(element).find("#clear").click(function(){
+        eventhandler.emit("clearCanvas");
+    });
 }
 
 function horsecursor(elem, emitter){
     function setBrushCursor(size){
         var adjust = parseInt(size/2);
-        $(elem).css('cursor', 'url(images/'+size+'x'+size+'.png) '+adjust+' '+adjust+', auto');
+        $(elem).css('cursor', 'url(images/brushes/'+size+'x'+size+'.png) '+adjust+' '+adjust+', auto');
     }
 
     function setStampCursor(stamp_url){
@@ -150,24 +184,32 @@ function horsecursor(elem, emitter){
     emitter.bind('setStamp', setStampCursor);
 }
 
-function bin(emitter){
+function bin(element, emitter){
     var duration = 150;
 
     function selectBin(toggle, group){
         console.log("selectBin", toggle, group);
-        $('.visible_bin').transition({x:"0px"}, duration, 'ease');
+        $(element).find('.visible_bin').transition({x:"0px"}, duration, 'ease');
         setTimeout(function(){
             $("."+group).hide();
             $("."+toggle).show();
         }, duration);
-        $('.visible_bin').transition({x:"200px"}, duration, 'ease');
+        $(element).find('.visible_bin').transition({x:"200px"}, duration, 'ease');
     }
 
     function closeBin(){
-        $('.visible_bin').transition({x:"0px"}, duration, 'ease');
+        $(element).find('.visible_bin').transition({x:"0px"}, duration, 'ease');
     }
     emitter.on("selectBin", selectBin);
     emitter.on("closeBin", closeBin);
+    
+    $(element).find(".toggle").click(function(el){
+        emitter.emit("selectBin", $(el.target).data('toggle'), $(el.target).data('group'));
+    });
+    
+    $(".main").click(function(el){
+        emitter.emit("closeBin")
+    });
 }
 
 function currentColor(element, emitter){
@@ -175,44 +217,417 @@ function currentColor(element, emitter){
         $(element).css({"background-color":newcolor});
     }
 
-    emitter.on("changeColor", changeColor)
+    emitter.on("changeColor", changeColor);
 }
 
-$(function() {
-    var emitter = new LucidJS.EventEmitter();
-    var canvas = document.getElementById("horse");
+function colorSelector(element, emitter){
+    var source = $("#color-template").html();
+    var template = Handlebars.compile(source);
 
-    bin(emitter);
-    horsecanvas(canvas, emitter);
-    horsecursor($('body'), emitter);
-    currentColor($('.current_color'), emitter);
-    
-    emitter.emit("changeSize", 10)
-
-    $("#done").click(function(){
-        emitter.emit("completeCanvas");
-    });
-    $("#clear").click(function(){
-        emitter.emit("clearCanvas");
-    });
-    $(".color").click(function(el){
+    var clickColor = function(el){
         emitter.emit("changeColor", $(el.target).data('color')) 
+        emitter.emit("changeSize", 10)
         emitter.emit("closeBin")
-    });
-    $(".brush").click(function(el){
+    }
+    
+    var addColorRow = function(color1, color1_name, color2, color2_name, color3, color3_name){
+        var color_row = template({'color1':color1, 
+                                  'color1_name':color1_name,
+                                  'color2':color2,
+                                  'color2_name':color2_name,
+                                  'color3':color3,
+                                  'color3_name':color3_name});
+        $(element).append($(color_row).click(clickColor));
+    }
+    
+    emitter.on("addColorRow", addColorRow);
+
+    $(element).find(".color").click(clickColor);
+    
+    $(element).find(".brush").click(function(el){
         emitter.emit("changeSize", parseInt($(el.target).data('size'), 10))
         emitter.emit("closeBin")
     });
-    $(".stamp").click(function(el){
+}
+
+function stampSelector(element, emitter){
+    var source = $("#stamp-template").html();
+    var template = Handlebars.compile(source);
+    
+    var clickStamp = function(el){
         emitter.emit("setStamp", $(el.target).data('url'));
         emitter.emit("closeBin")
-    });
-    $(".background").click(function(el){
+    }
+
+    var addStamp = function(url, title){
+        var stamp_rendered = template({'url': url, 'title':title});
+        $(element).append($(stamp_rendered).click(clickStamp));
+    }
+
+    emitter.on("addStamp", addStamp);
+
+    $(element).find(".stamp").click(clickStamp);
+}
+
+function backgroundSelector(element, emitter){
+    var source = $("#background-template").html();
+    var template = Handlebars.compile(source);
+
+    var clickBackground = function(el){
         emitter.emit("setBackground", $(el.target).data('url'));
         emitter.emit("closeBin")
+    }
+
+    var addBackground = function(url, title){
+        var background_rendered = template({'url': url, 'title':title});
+        $(element).append($(background_rendered).click(clickBackground));
+    }
+
+    emitter.on("addBackground", addBackground);
+    
+    $(element).find(".background").click(clickBackground);
+}
+
+function stableSelector(element, emitter){
+    var source = $("#stable-template").html();
+    var template = Handlebars.compile(source);
+
+    var clickStable = function(el){
+        emitter.emit("clearCanvas");
+        emitter.emit("setBackground", $(el.target).data('url'));
+    }
+
+    var newImage = function(img){
+        var img_rendered = template({'img': img});
+        $(element).append($(img_rendered).click(clickStable));
+    }
+
+    emitter.on("newImage", newImage);
+    $(element).find('.stable').click(clickStable);
+
+}
+
+function storeManager(element, emitter){
+    var source = $("#store-template").html();
+    var template = Handlebars.compile(source);
+    
+    var items = { 'argy':
+        {
+            'id': 'argy',
+            'price': 0, 
+            'name': 'Argy Bee', 
+            'stamp_url':'images/stamps/argy_bee.png', 
+            'stamp_name':'argy_bee',
+            'color1': '#ff0000',
+            'color1_name': 'Red',
+            'color2': '#00ff00',
+            'color2_name': 'Green',
+            'color3': '#0000ff',
+            'color3_name': 'Blue',
+            'background_url': 'images/backgrounds/photoshop.png',
+            'background_name': 'Photosharp'
+        },
+        'palomino':
+        {
+            'id':'palomino',
+            'price': 9,
+            'name': 'Palomino', 
+            'stamp_url': 'images/stamps/bw_horse_3.png',
+            'stamp_name': 'Palomino',
+            'color1': '#d88e45', 
+            'color1_name': 'Palomino Dark',
+            'color2': '#f6dbbd',
+            'color2_name': 'Palomino Light',
+            'color3': '#fefaf7',
+            'color3_name': 'Palomino Blonde',
+            'background_url': 'images/backgrounds/palomino.png',
+            'background_name': 'Palomino',
+        },
+        'bay':
+        {
+            'id':'bay',
+            'price': 16,
+            'name': 'Bay', 
+            'stamp_url': 'images/stamps/bw_horse_4.png',
+            'stamp_name': 'Bay',
+            'color1': '#6d3e20', 
+            'color1_name': 'Bay Dark',
+            'color2': '#d8a578',
+            'color2_name': 'Bay Light',
+            'color3': '#302f2b',
+            'color3_name': 'Black Hair',
+            'background_url': 'images/backgrounds/bay.png',
+            'background_name': 'Bay',
+        },
+        'chestnut':
+        {
+            'id':'chestnut',
+            'price': 16,
+            'name': 'Chestnut', 
+            'stamp_url': 'images/stamps/bw_horse_5.png',
+            'stamp_name': 'Chestnut',
+            'color1': '#774c3c', 
+            'color1_name': 'Chestnut Dark',
+            'color2': '#f3b890',
+            'color2_name': 'Chestnut Light',
+            'color3': '#503232',
+            'color3_name': 'Chestnut Hair',
+            'background_url': 'images/backgrounds/chestnut.png',
+            'background_name': 'Chestnut',
+        },
+        'twilight':
+        {
+            'id':'twilight',
+            'price': 9,
+            'name': 'Twilight', 
+            'stamp_url': 'images/stamps/twilight.png',
+            'stamp_name': 'Mark',
+            'color1': '#d098e4', 
+            'color1_name': 'Purple',
+            'color2': '#24386d',
+            'color2_name': 'Dark Blue',
+            'color3': '#f64188',
+            'color3_name': 'Pink',
+            'background_url': 'images/backgrounds/little_pony.png',
+            'background_name': 'Chestnut',
+        }
+    };
+    var item_id_list = ['argy', 'palomino', 'bay', 'chestnut', 'twilight'] 
+
+    var addItem = function( obj ){
+        var id = obj['id'];
+        var price = obj['price'];
+        var name = obj['name'];
+        var stamp_url = obj['stamp_url'];
+        var stamp_name = obj['stamp_name'];
+        var color1 = obj['color1'];
+        var color1_name = obj['color1_name'];
+        var color2 = obj['color2'];
+        var color2_name = obj['color2_name'];
+        var color3 = obj['color3']
+        var color3_name = obj['color3_name'];
+        var background_url = obj['background_url'];
+        var background_name = obj['background_name'];
+
+        var item_rendered = template(obj);
+        $(element).append(item_rendered);
+    }
+
+    var update = function( money ){
+        // each store_item, if price < money, show lock.  
+        $.each(item_id_list, function(i, id){
+            var element = $("#"+id);
+            var record = items[id];
+            if( record.price <= money ){
+                element.find(".visible_when_available").show();
+                element.find(".lock").hide();
+            }
+            else{
+                element.find(".visible_when_available").hide();
+                element.find(".lock").show();
+            }
+        });
+    }
+
+    var _pay = function(id){
+        var record = items[id];
+        emitter.emit("payMoney", record.price);
+    }
+
+    var _buy = function(id){
+        var element = $("#"+id);
+        var record = items[id];
+        emitter.emit("addColorRow", record.color1, record.color1_name,
+                                    record.color2, record.color2_name,
+                                    record.color3, record.color3_name);
+        emitter.emit("addStamp", record.stamp_url, record.stamp_name);
+        emitter.emit("addBackground", record.background_url, record.background_name);
+        element.transition({'scale':0.1}, 500, 'ease');
+        setTimeout(function(){element.hide()}, 500)
+    }
+
+    var buy = function(id){
+        _buy(id);
+        _pay(id);
+    }
+
+    var buyWithoutPaying = function( id ){
+        _buy(id);
+    }
+
+    emitter.on("buy", buy);
+    emitter.on("buyWithoutPaying", buyWithoutPaying);
+    emitter.on("update", update);
+
+
+    $.each(item_id_list, function(i, id){
+        addItem(items[id]);
     });
-    $(".toggle").click(function(el){
-        emitter.emit("selectBin", $(el.target).data('toggle'), $(el.target).data('group'));
+    update(0);
+    
+    $(element).find(".storebutton").click(function(el){
+        emitter.emit("buy", $(el.target).data('id'));
     });
+    
+}
+
+function chartModal(emitter){
+    var source = $("#modal-template").html();
+    var template = Handlebars.compile(source);
+
+    var shoo = function( img, analysis, horsedollars ){
+        $("#modal").transition({scale:'0.5'}, 500).transition({x:'-1700px'}, 500);
+        setTimeout(function(){
+            $("#modal").remove();
+            $("#overlay").remove();
+            }, 1200);
+    }
+
+    var showBreakdown = function( img, analysis, horsedollars ){
+        var rendered_template = template({'img': img,
+                                          'horsedollars': horsedollars});
+        $('.main').append( rendered_template );
+        var context = $('.horsechart')[0].getContext("2d");
+
+        var colorhistogram = analysis.colorhistogram;
+        var data = [];
+        for (var key in colorhistogram) {
+            if (colorhistogram.hasOwnProperty(key)) {
+                data.push({
+                    value:colorhistogram[key], 
+                    color:key,
+                    label:key,
+                    highlight:key,
+                });
+            }
+        }
+        var newChart = new Chart(context).Doughnut(data, {});
+
+        $("#modal").click(shoo);
+        $("#overlay").click(shoo);
+
+    }
+
+    emitter.on('showBreakdown', showBreakdown);
+}
+
+function horseDollaDollaBill(element, emitter){
+    var money = 0;
+
+    var payMoney = function(amount){
+        console.log("paying money", amount);
+        money = money - amount;
+        element.html(money);
+        emitter.emit('update', money);
+
+    }
+    var addMoney = function(amount){
+        console.log("adding money", amount);
+        money = money + amount;
+        element.html(money);
+        emitter.emit('update', money);
+    }
+    var setMoney = function(amount){
+        console.log("setmoney", amount);
+        money = amount;
+        element.html(money);
+        emitter.emit('update', money);
+    }
+
+    emitter.on('payMoney', payMoney);
+    emitter.on('addMoney', addMoney);
+    emitter.on('setMoney', setMoney);
+}
+
+function stateRunRadio(element, emitter){
+    // Save all events that change the page's state. 
+    // replay them on page load. 
+    var set = function(key,value){
+        localStorage[key] = JSON.stringify(value);
+    }
+    var get = function(key){
+        if(localStorage[key] !== undefined){
+            return JSON.parse(localStorage[key]);
+        }
+        else{
+            return undefined;
+        }
+    }
+
+    var updateMoney = function(money){
+        set('money', money);
+    }
+    var addPurchase = function(purchase){
+        var purchases = get('purchases');
+        if(purchases === undefined){
+            purchases = [];
+        }
+        purchases.push(purchase)
+        set('purchases', purchases);
+    }
+    var addImage = function(img){
+        var images = get('images');
+        if(images === undefined){
+            images = [];
+        }
+        images.push(img);
+        set('images', images);
+    }
+    var reset = function(){
+        set('money', 0);
+        set('purchases', []);
+        set('images', []);
+        location.reload();
+    }
+    
+    var loadedMoney = get('money');
+    if (loadedMoney !== undefined){
+        console.log('setting money:', loadedMoney);
+        emitter.emit('setMoney', loadedMoney);
+    }
+    var loadedPurchases = get('purchases');
+    if (loadedPurchases !== undefined){
+        $.each(loadedPurchases, function(i, purchase){
+            emitter.emit('buyWithoutPaying', purchase);
+        });
+    }
+    var loadedImages = get('images');
+    if (loadedImages !== undefined){
+        $.each(loadedImages, function(i, image){
+            emitter.emit('newImage', image);
+        });
+    }
+
+    emitter.on('update', updateMoney);
+    emitter.on('buy', addPurchase);
+    emitter.on('newImage', addImage);
+    emitter.on('reset', reset);
+
+    element.click(reset);
+
+}
+
+
+
+$(function() {
+    var emitter = new LucidJS.EventEmitter();
+    
+    horsecanvas($('.main'), emitter);
+    horsecursor($('.main'), emitter);
+
+    bin($('.left'), emitter);
+    currentColor($('.current_color'), emitter);
+    colorSelector($('.colors'), emitter);
+    stampSelector($('.stamps'), emitter);
+    backgroundSelector($('.backgrounds'), emitter);
+    stableSelector($('.stables'), emitter);
+    storeManager($('.store'), emitter);
+    horseDollaDollaBill($('.horsecounter'), emitter);
+    chartModal(emitter);
+    stateRunRadio($(".reset"), emitter);
+    
+    emitter.emit("changeSize", 10);
+    emitter.emit("setBackground", "images/backgrounds/photoshop.png");
+
 });
 
